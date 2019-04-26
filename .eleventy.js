9f6e3d82c4dba61936a6e9bf5952ade180638930
Zeke Farwell <zeke@gearx.com>
2019-04-26 00:54:51 -0400

    initial conversion of sessions template

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,19 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/js");
     eleventyConfig.addPassthroughCopy("src/media");
 
+
+    eleventyConfig.addFilter("to12hourTime", function(timeString) { 
+        let date = new Date(timeString);
+        let time = date.toLocaleTimeString('en-US', {
+            hour12: true,
+            hour: 'numeric',
+            minute: 'numeric',
+        })
+        return time;
+    });
+
+
+
     return {
         dir: {
             input: "src",
