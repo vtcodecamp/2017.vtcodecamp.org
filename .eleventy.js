f51abb4cc27270be4a6de416906b1b261154c333
KyleMit <kylemit@gmail.com>
2019-05-05 16:33:11 -0400

    add favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
 
+    eleventyConfig.addPassthroughCopy("src/favicon.ico");
     eleventyConfig.addPassthroughCopy("src/css");
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/js");
